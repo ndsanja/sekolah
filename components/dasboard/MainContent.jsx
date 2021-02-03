bab8188c32f264852eb11af061a76bfefa848d34
ndsanja <ndsanja@gmail.com>
2021-02-03 23:38:15 +0700

    penyesuaian padding container dashboard

--- components/dasboard/MainContent.jsx
+++ components/dasboard/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <div className="space-y-4 text-justify w-8/12 p-8 overflow-y-auto bg-gray-50 shadow-2xl">
+    <div className="space-y-4 text-justify w-8/12 p-8 overflow-y-auto bg-gray-50 shadow-xl">
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero officiis,
         eligendi possimus consequatur neque exercitationem eaque dolore ut eius
--- components/dasboard/RightBar.jsx
+++ components/dasboard/RightBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function RightBar() {
   return (
-    <div className="p-4 text-sm leading-relaxed  w-2/12 overflow-y-auto space-y-4 bg-gray-50 shadow-xl">
+    <div className="p-4 text-sm leading-relaxed  w-3/12 overflow-y-auto space-y-4 bg-gray-50">
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolorum
         necessitatibus laboriosam cupiditate laborum cumque id mollitia et nisi.
