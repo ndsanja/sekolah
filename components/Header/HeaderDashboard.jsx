cec2f85b191ef22b4caab2a7a674736118b41ae0
ndsanja <ndsanja@gmail.com>
2021-02-07 19:49:18 +0700

    dark mode

--- components/Header/HeaderDashboard.jsx
+++ components/Header/HeaderDashboard.jsx
@@ -6,7 +6,7 @@ export default function HeaderDashboard() {
   const [open, isOpen] = useState(true);
   const [dark, setDark] = useState(true);
 
-  const toogleDark = () => setDark(!dark);
+  // const toogleDark = () => setDark(!dark);
   const toogleOpen = () => isOpen(!open);
   const selectTheme = (value) => {
     localStorage.setItem("theme", value);
@@ -14,10 +14,10 @@ export default function HeaderDashboard() {
     html.classList.add(localStorage.getItem("theme"));
     if (value == "dark") {
       html.classList.remove("light");
-      setDark((dark = true));
+      setDark((dark = false));
     } else {
       html.classList.remove("dark");
-      setDark((dark = false));
+      setDark((dark = true));
     }
   };
 
@@ -68,49 +68,47 @@ export default function HeaderDashboard() {
               </div>
             </div>
             <div className="flex items-center space-x-1">
-              <div onClick={toogleDark}>
-                {dark ? (
-                  <button
-                    onClick={() => selectTheme("dark")}
-                    className="hover:bg-blue-50 dark:hover:bg-blue-800 p-2 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
+              {dark ? (
+                <button
+                  onClick={() => selectTheme("dark")}
+                  className="hover:bg-blue-50 dark:hover:bg-blue-800 p-2 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
+                >
+                  <svg
+                    className="h-6 w-6 text-gray-700 dark:text-blue-100"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                   >
-                    <svg
-                      className="h-6 w-6 text-gray-700 dark:text-blue-100"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                      />
-                    </svg>
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => selectTheme("light")}
-                    className="hover:bg-blue-50 dark:hover:bg-blue-800 p-2 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  </svg>
+                </button>
+              ) : (
+                <button
+                  onClick={() => selectTheme("light")}
+                  className="hover:bg-blue-50 dark:hover:bg-blue-800 p-2 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
+                >
+                  <svg
+                    className="h-6 w-6 text-gray-700 dark:text-blue-100"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                   >
-                    <svg
-                      className="h-6 w-6 text-gray-700 dark:text-blue-100"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                      />
-                    </svg>
-                  </button>
-                )}
-              </div>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                </button>
+              )}
 
               <a href="#">
                 <div className="hover:bg-blue-50 dark:hover:bg-blue-800 p-2 rounded-full flex items-center justify-center">
@@ -146,7 +144,6 @@ export default function HeaderDashboard() {
             open={open}
             selectTheme={selectTheme}
             dark={dark}
-            toogleDark={toogleDark}
           />
         </div>
         <MainDashboard />
