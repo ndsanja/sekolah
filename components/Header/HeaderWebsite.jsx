346c045d3d1f790334b72e960a758bcfdca5a205
ndsanja <ndsanja@gmail.com>
2021-02-08 22:42:15 +0700

    membuat homepage website

--- components/Header/HeaderWebsite.jsx
+++ components/Header/HeaderWebsite.jsx
@@ -6,11 +6,11 @@ export default function HeaderWebsite() {
       <div className="fixed top-0 left-0 right-0 bg-blueDark">
         <div className="container px-4 flex justify-end items-center relative">
           <div className="absolute left-2 top-1 shadow-2xl">
-            <img src="/logo.png" alt="logo" className="w-14" />
+            <img src="/logo.png" alt="logo" className="w-12" />
           </div>
 
           <div className="flex items-center justify-between text-gray-50 space-x-4">
-            <div className="flex items-center space-x-2 px-2 py-3 border-gray-600 border-l border-r">
+            <div className="flex items-center space-x-2 px-2 py-2.5 border-gray-600 border-l border-r">
               <h1 className="uppercase">Menu</h1>
               <div>
                 <Icon.Menu width={2} className="h-7 w-7 bg-contain" />
